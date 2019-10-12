d48deec5241770fd12fb9e29cd0c6ccecc29913f
liuyichong <jiuliuwangdao@foxmail.com>
2019-10-12 08:10:55 +0800

    update

---
+++ IO/BufferedReader.java
@@ -0,0 +1,24 @@
+﻿import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.BufferedReader;
+
+public class File_BufferedReader {
+	public static void main(String[] args) {
+		File f1=new File("F:/Java_IO/Class1/HelloIO2.txt");
+		try (
+			FileReader fr=new FileReader(f1);
+				//缓存流必须建立在一个存在的流的基础上
+			BufferedReader br=new BufferedReader(fr);
+			)
+		{
+			while(true) {
+				String line=br.readLine();
+				if(null==line)break;
+				System.out.println(line);
+			}
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ IO/PrintWriter.java
@@ -0,0 +1,26 @@
+﻿import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.IOException;
+
+public class File_PrintWriter {
+	public static void main(String[] args) {
+		File f1=new File("F:/Java_IO/Class1/HelloIO2.txt");
+		
+		try (
+			FileWriter fw=new FileWriter(f1);
+			PrintWriter pw=new PrintWriter(fw);
+			)
+		{
+			pw.println("I'm learning Java now!");
+			pw.flush();
+			pw.println("I love Java and I'm making great progress.");
+			pw.flush();
+			pw.println("Don't worry,everything is just fine!");
+			pw.flush();
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
