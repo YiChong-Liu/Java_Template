3ca3a502019f28845bb3e5c1725136e2803c1df0
liuyichong <jiuliuwangdao@foxmail.com>
2019-10-20 15:49:24 +0800

    定义一个Hero类，以供练习

---
+++ Collections_Framework/Hero.java
@@ -0,0 +1,26 @@
+﻿package Collections_Framework;
+
+public class Hero {
+	public String name;
+	private double hp;
+	public int damage;
+	
+	public Hero(String name) {
+		this.name=name;
+	}
+	public double getHp() {
+		return hp;
+	}
+	public void setHp(double hp) {
+		this.hp = hp;
+	}
+	
+	public String toString() {
+		return name;
+	}
+	public static void main(String[] args) {
+		
+	}
+
+
+}
