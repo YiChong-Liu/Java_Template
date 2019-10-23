8694257e7e72ee26af80b52c4f31a1a31c0bc74f
liuyichong <jiuliuwangdao@foxmail.com>
2019-10-23 11:27:29 +0800

    hashMap的基本操作

---
+++ Collections_Framework/_hashMap.java
@@ -0,0 +1,16 @@
+﻿import java.util.HashMap; 
+public class _hashMap {
+	public static void main(String[] args) {
+		HashMap<String,String>candidates=new HashMap<>();
+		candidates.put("1", "Andrew Yang");
+		candidates.put("2", "Elizabeth Warren");
+		candidates.put("3","Joe Biden");
+		candidates.put("3","Bernie Sanders");
+		System.out.println(candidates.get("1"));
+		//"Joe Biden"被覆盖
+		System.out.println(candidates.get("3"));
+		candidates.put("4","Andrew Yang");
+		System.out.println(candidates);
+		candidates.clear();
+	}
+}
