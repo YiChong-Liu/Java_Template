c3662ae68cfeafd83b08d88a398901600099117f
liuyichong <jiuliuwangdao@foxmail.com>
2019-11-02 20:20:38 +0800

    update

---
+++ lambda/Hero.java
@@ -0,0 +1,31 @@
+﻿package lambda;
+
+public class Hero {
+	public String name;
+	public double hp;
+	public int damage;
+	
+	public Hero(String name) {
+		this.name=name;
+	}
+
+	public Hero(String name,double hp,int damage) {
+		this.name=name;
+		this.hp=hp;
+		this.damage=damage;
+	}
+	
+	public String toString() {
+		return name;
+	}
+	
+	//引用容器中的对象
+	public boolean matched() {
+		return this.hp>99&&this.damage<49;
+	}
+	public static void main(String[] args) {
+		
+	}
+
+
+}
---
+++ lambda/HeroChecker.java
@@ -0,0 +1,6 @@
+﻿package lambda;
+
+import lambda.Hero;
+public interface HeroChecker {
+	public boolean test(Hero h);
+}
